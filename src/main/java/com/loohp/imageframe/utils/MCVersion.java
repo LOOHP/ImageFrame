c6a92dc7ce87cddff3abd210120dc040fc52c224
LOOHP <jamesloohp@gmail.com>
2023-06-08 17:35:00 +0100

    Minecraft 1.20

--- src/main/java/com/loohp/imageframe/utils/MCVersion.java
+++ src/main/java/com/loohp/imageframe/utils/MCVersion.java
@@ -1,5 +1,5 @@
 /*
- * This file is part of InteractiveChat.
+ * This file is part of ImageFrame.
  *
  * Copyright (C) 2022. LoohpJames <jamesloohp@gmail.com>
  * Copyright (C) 2022. Contributors
@@ -20,45 +20,49 @@
 
 package com.loohp.imageframe.utils;
 
+import java.util.Arrays;
+
 public enum MCVersion {
 
-    V1_19_4("1.19.4", 20),
-    V1_19_3("1.19.3", 19),
-    V1_19("1.19", 18),
-    V1_18_2("1.18.2", 17),
-    V1_18("1.18", 16),
-    V1_17("1.17", 15),
-    V1_16_4("1.16.4", 14),
-    V1_16_2("1.16.2", 13),
-    V1_16("1.16", 12),
-    UNSUPPORTED("Unsupported", -1);
+    V1_20("1.20", "1_20_R1", 21),
+    V1_19_4("1.19.4", "1_19_R3", 20),
+    V1_19_3("1.19.3", "1_19_R2", 19),
+    V1_19("1.19", "1_19_R1", 18),
+    V1_18_2("1.18.2", "1_18_R2", 17),
+    V1_18("1.18", "1_18_R1", 16),
+    V1_17("1.17", "1_17_R1", 15),
+    V1_16_4("1.16.4", "1_16_R3", 14),
+    V1_16_2("1.16.2", "1_16_R2", 13),
+    V1_16("1.16", "1_16_R1", 12),
+    V1_15("1.15", "1_15_R1", 11),
+    V1_14("1.14", "1_14_R1", 10),
+    V1_13_1("1.13.1", "1_13_R2", 9),
+    V1_13("1.13", "1_13_R1", 8),
+    V1_12("1.12", "1_12_R1", 7),
+    V1_11("1.11", "1_11_R1", 6),
+    V1_10("1.10", "1_10_R1", 5),
+    V1_9_4("1.9.4", "1_9_R2", 4),
+    V1_9("1.9", "1_9_R1", 3),
+    V1_8_4("1.8.4", "1_8_R3", 2),
+    V1_8_3("1.8.3", "1_8_R2", 1),
+    V1_8("1.8", "1_8_R1", 0),
+    UNSUPPORTED("Unsupported", null, -1);
+
+    public static final MCVersion MINIMUM_SUPPORTED_VERSION = V1_16;
+
+    private static final MCVersion[] SUPPORTED_VALUES = Arrays.stream(values()).filter(v -> v.isSupported()).toArray(MCVersion[]::new);
 
     public static MCVersion fromPackageName(String packageName) {
-        if (packageName.contains("1_19_R3")) {
-            return V1_19_4;
-        } else if (packageName.contains("1_19_R2")) {
-            return V1_19_3;
-        } else if (packageName.contains("1_19_R1")) {
-            return V1_19;
-        } else if (packageName.contains("1_18_R2")) {
-            return V1_18_2;
-        } else if (packageName.contains("1_18_R1")) {
-            return V1_18;
-        } else if (packageName.contains("1_17_R1")) {
-            return V1_17;
-        } else if (packageName.contains("1_16_R3")) {
-            return V1_16_4;
-        } else if (packageName.contains("1_16_R2")) {
-            return V1_16_2;
-        } else if (packageName.contains("1_16_R1")) {
-            return V1_16;
-        } else {
-            return UNSUPPORTED;
+        for (MCVersion version : SUPPORTED_VALUES) {
+            if (packageName.contains(version.getPackageName())) {
+                return version;
+            }
         }
+        return UNSUPPORTED;
     }
 
     public static MCVersion fromNumber(int number) {
-        for (MCVersion version : values()) {
+        for (MCVersion version : SUPPORTED_VALUES) {
             if (version.shortNum == number) {
                 return version;
             }
@@ -67,16 +71,22 @@ public enum MCVersion {
     }
 
     private final String name;
+    private final String packageName;
     private final int shortNum;
 
-    MCVersion(String name, int shortNum) {
+    MCVersion(String name, String packageName, int shortNum) {
         this.name = name;
+        this.packageName = packageName;
         this.shortNum = shortNum;
     }
 
     @Override
     public String toString() {
         return name;
+    }
+
+    public String getPackageName() {
+        return packageName;
     }
 
     public int getNumber() {
@@ -114,12 +124,21 @@ public enum MCVersion {
         }
     }
 
+    public boolean isLegacy() {
+        return isOlderOrEqualTo(V1_12);
+    }
+
+    public boolean isOld() {
+        return isOlderOrEqualTo(V1_8_4);
+    }
+
     public boolean isSupported() {
-        return this.shortNum >= 0;
+        return this.shortNum >= MINIMUM_SUPPORTED_VERSION.shortNum;
     }
 
     public boolean isLegacyRGB() {
-        return isOlderThan(MCVersion.V1_16);
+        return isOlderThan(V1_16);
     }
 
 }
+
